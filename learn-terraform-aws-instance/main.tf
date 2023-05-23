ec8a69a8f00c692f7481e1fc60e9fd37b4419475
geeshka <gerchukekaterina@gmail.com>
2023-05-24 01:40:14 +0300

    first commit

---
+++ learn-terraform-aws-instance/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  cloud {
+    organization = "ACME_LLC"
+    workspaces {
+      name = "learn-tfc-aws"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "eu-central-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-04e601abe3e1a910f"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
